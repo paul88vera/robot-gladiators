8f997884c56c4a8cc9919d396c155e1ed7b60f71
paul88vera <paul88vera@gmail.com>
2021-11-17 20:37:25 -0600

    initial game setup

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,7 @@
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
+var playerMoney = 10;
 
 console.log(playerName, playerAttack, playerHealth);
 
@@ -9,28 +10,55 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 
 var fight = function() {
-  window.alert("Welcome to Robot Gladiators!")
-  enemyHealth = enemyHealth - playerAttack;
+  window.alert("Welcome to Robot Gladiators!");
+  var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+
+// does player want to fight or skip
+if (promptFight === 'FIGHT' || promptFight === 'fight') {
+  // player attacks enemy
+  enemyHealth -= playerAttack;
   console.log(
     playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
   );
 
+  //Check enemy's health
   if (enemyHealth <= 0) {
     window.alert(enemyName + " has died!");
   } else {
     window.alert(enemyName + " still has " + enemyHealth + " health left.");
   }
 
-  playerHealth = playerHealth - enemyAttack;
+  //Enemy attacks player
+  playerHealth -= enemyAttack;
   console.log(
     enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
   );
 
+  // Check player's health
   if (playerHealth <= 0) {
     window.alert(playerName + " has died!");
   } else {
     window.alert(playerName + " still has " + playerHealth + " health left.");
   }
+
+  // if player chooses to skip
+} else if (promptFight === 'skip' || promptFight === 'SKIP') {
+  //confirm player wants to skip
+  var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+    
+  // if true, leave the fight
+  if (confirmSkip){
+    window.alert(playerName + " has decided to skip this fight. Goodbye!");
+  // subtract money for skipping
+    playerMoney -= 2;
+  }
+  // if no (false), ask question again by running fight() again
+  else {
+  fight();
+  }
+} else {
+  window.alert("You need to choose a valid option. Try again!");
+}
 }
 
 fight();
